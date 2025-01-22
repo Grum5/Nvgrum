7e354a3c53d1cc22850367dfe1ed2c8cd05ce6f9
Grum5 <al22760591@ite.edu.mx>
2025-01-22 01:06:23 -0800

    Changes

--- lua/lsp/lsp_servers.lua
+++ lua/lsp/lsp_servers.lua
@@ -1,6 +1,6 @@
 local lspconfig = require('lspconfig')
 
-lspconfig.lua_ls.setup({
+lspconfig.lua_ls.setup({    -- lua_ls server setup Lua
     settings = {
         Lua = {
             diagnostics = {
@@ -9,3 +9,11 @@ lspconfig.lua_ls.setup({
         },
     },
 })
+
+lspconfig.pylsp.setup({})   -- pylsp server setup Python
+
+lspconfig.clangd.setup({})  -- clangd server setup C++
+
+lspconfig.ts_ls.setup({})   -- ts_ls server setup Typescript and Javascript
+
+lspconfig.astro.setup({})
--- lua/lsp/mason_setup.lua
+++ lua/lsp/mason_setup.lua
@@ -1,8 +1,18 @@
 local mason = require('mason')
 local mason_lspconfig = require('mason-lspconfig')
 
+-- Verificar que esten instalados los servidores
+local servers = {
+    'clangd',   -- C++ server
+    'pylsp',    -- Python server
+    'lua_ls',   -- Lua server
+    'ts_ls',    -- Javascript and Typescript server
+    'astro'     -- Astro Framework server
+}
+
+-- Mason setup
 mason.setup()
 mason_lspconfig.setup({
-    ensure_installed = { "clangd", "pylsp", "lua_ls" },
+    ensure_installed = servers,
     automatic_installation = true,
 })
