89eb2909d827443b79dd63838dcf757bc0aad401
Grum5 <al22760591@ite.edu.mx>
2025-01-29 15:48:01 -0800

    adding c# LSP

--- lua/lsp/mason_lspconfig.lua
+++ lua/lsp/mason_lspconfig.lua
@@ -12,7 +12,8 @@ local servers = {
     'ts_ls',                    -- Javascript and Typescript LSP
     'astro',                    -- Astro LSP
     'kotlin_language_server',   -- Kotlin LSP
-    'gradle_ls'                 -- Gradle LSP
+    'gradle_ls',                -- Gradle LSP
+    'omnisharp',                -- C# LSP
 }
 
 -- ---------------------------------------------------
@@ -28,23 +29,23 @@ local on_attach = function(client, bufnr)
     -- Navegación y ayuda
     buf_set_keymap("n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)       -- Ir a la definición
     buf_set_keymap("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts)     -- Ir a la declaración
-    buf_set_keymap("n", "gr", "<Cmd>lua vim.lsp.buf.references()<CR>", opts)      -- Mostrar referencias
-    buf_set_keymap("n", "gi", "<Cmd>lua vim.lsp.buf.implementation()<CR>", opts) -- Ir a implementación
+    -- buf_set_keymap("n", "gr", "<Cmd>lua vim.lsp.buf.references()<CR>", opts)      -- Mostrar referencias
+    -- buf_set_keymap("n", "gi", "<Cmd>lua vim.lsp.buf.implementation()<CR>", opts) -- Ir a implementación
     buf_set_keymap("n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)            -- Mostrar documentación flotante
     buf_set_keymap("n", "<C-k>", "<Cmd>lua vim.lsp.buf.signature_help()<CR>", opts) -- Ayuda de firma
 
     -- Diagnósticos
     buf_set_keymap("n", "[d", "<Cmd>lua vim.diagnostic.goto_prev()<CR>", opts)    -- Ir al diagnóstico previo
     buf_set_keymap("n", "]d", "<Cmd>lua vim.diagnostic.goto_next()<CR>", opts)    -- Ir al siguiente diagnóstico
-    buf_set_keymap("n", "<leader>e", "<Cmd>lua vim.diagnostic.open_float()<CR>", opts) -- Mostrar diagnóstico actual
-    buf_set_keymap("n", "<leader>q", "<Cmd>lua vim.diagnostic.setloclist()<CR>", opts) -- Añadir diagnósticos al local list
+    -- buf_set_keymap("n", "<leader>e", "<Cmd>lua vim.diagnostic.open_float()<CR>", opts) -- Mostrar diagnóstico actual
+    -- buf_set_keymap("n", "<leader>q", "<Cmd>lua vim.diagnostic.setloclist()<CR>", opts) -- Añadir diagnósticos al local list
 
     -- Acciones de código
-    buf_set_keymap("n", "<leader>rn", "<Cmd>lua vim.lsp.buf.rename()<CR>", opts)      -- Renombrar variable
-    buf_set_keymap("n", "<leader>ca", "<Cmd>lua vim.lsp.buf.code_action()<CR>", opts) -- Ejecutar acción de código
+    -- buf_set_keymap("n", "<leader>rn", "<Cmd>lua vim.lsp.buf.rename()<CR>", opts)      -- Renombrar variable
+    -- buf_set_keymap("n", "<leader>ca", "<Cmd>lua vim.lsp.buf.code_action()<CR>", opts) -- Ejecutar acción de código
 
     -- Formateo
-    buf_set_keymap("n", "<leader>f", "<Cmd>lua vim.lsp.buf.format({ async = true })<CR>", opts) -- Formatear el archivo actual
+    -- buf_set_keymap("n", "<leader>f", "<Cmd>lua vim.lsp.buf.format({ async = true })<CR>", opts) -- Formatear el archivo actual
 end
 
 -- ---------------------------------------------------
@@ -90,4 +91,7 @@ lspconfig.astro.setup({})                   -- astro LSP setup | Astro Framework
 lspconfig.kotlin_language_server.setup({})  -- kotlin_language_server LSP setup | Kotlin
 
 lspconfig.gradle_ls.setup({})               -- gradle_ls LSP setup | Gradle
+
+lspconfig.omnisharp.setup({})               -- omnisharp LSP setup | C#
+
 
