c4fcc7fbccfe0ae49110628cf3132d0e4ea9caeb
Grum5 <al22760591@ite.edu.mx>
2025-02-07 08:04:17 -0800

    Cambios esteticos

--- lua/estetica/winbar.lua
+++ lua/estetica/winbar.lua
@@ -1,14 +1,7 @@
+
 require("bufferline").setup {
   options = {
     numbers = "none",               -- No mostrar números en los buffers
-    close_command = nil,            -- Deshabilitar el comando de cierre con el mouse
-    right_mouse_command = nil,      -- Deshabilitar el comando con el clic derecho
-    left_mouse_command = "buffer %d",  -- Cambiar de buffer con el clic izquierdo (sin usar mouse)
-    middle_mouse_command = nil,     -- Desactivar clic del medio
-    indicator = {
-      style = "icon",               -- Usar un icono como indicador del buffer activo
-      icon = "▎",                   -- Indicador de buffer activo
-    },
     buffer_close_icon = "",         -- Sin icono para cerrar el buffer
     modified_icon = "●",            -- Icono para buffers modificados
     close_icon = "",                -- Sin icono para el botón de cerrar
@@ -17,15 +10,27 @@ require("bufferline").setup {
     max_name_length = 18,           -- Longitud máxima del nombre de los archivos
     max_prefix_length = 15,         -- Longitud máxima del nombre truncado
     tab_size = 18,                  -- Tamaño de las pestañas
+    highlights = {
+        fill = {
+            bg = {
+                attribute = "fg",
+                highlight = "Pmenu"
+            }
+        }
+    },
     diagnostics = "nvim_lsp",       -- Mostrar diagnósticos LSP
     diagnostics_update_in_insert = false, -- No actualizar diagnósticos en modo insert
     custom_filter = function(buf_number, buf_numbers)
-      return true  -- Mostrar todos los buffers sin restricciones
+        local buf_name = vim.fn.bufname(buf_number)
+        if vim.fn.isdirectory(buf_name) == 1 then
+            return false
+        end
+        return true  -- Mostrar todos los buffers sin restricciones
     end,
-    offsets = {{filetype = "NvimTree", text = "File Explorer", padding = 1}},  -- Ajustes para NvimTree si lo usas
     show_buffer_icons = true,       -- Mostrar iconos para los archivos
     show_buffer_close_icons = false, -- Deshabilitar los iconos de cerrar
     show_tab_indicators = false,    -- No mostrar indicadores de pestañas
     persist_buffer_sort = true,     -- Mantener el orden de los buffers
   },
 }
+
