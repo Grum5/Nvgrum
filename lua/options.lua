5eaa5b004a81b9b99dbbb27396ab80bcd0a4f829
Grum5 <al22760591@ite.edu.mx>
2025-02-07 08:04:24 -0800

    Cambios esteticos

--- lua/options.lua
+++ lua/options.lua
@@ -1,22 +1,23 @@
 -- ~/.config/nvim/lua/options.lua
 
 -- Opciones generales
-vim.opt.number = true             -- Mostrar números de línea
-vim.opt.relativenumber = true     -- Números relativos
-vim.opt.expandtab = true          -- Usar espacios en vez de tabulaciones
-vim.opt.shiftwidth = 4            -- Espacios por nivel de indentación
-vim.opt.tabstop = 4               -- Tamaño de un tab
-vim.opt.smartindent = true        -- Indentación inteligente
-vim.opt.hlsearch = false          -- Desactivar el resaltado de búsqueda
-vim.opt.incsearch = true          -- Búsqueda incremental
-vim.opt.termguicolors = true      -- Colores en terminal
-vim.opt.cmdheight = 2             -- Altura de la línea de comandos
-vim.opt.laststatus = 2            -- Siempre mostrar la barra de estado.
-vim.opt.ignorecase = true         -- Ignorar mayúsculas/minúsculas en búsquedas
-vim.opt.smartcase = true          -- Activar búsqueda sensible a mayúsculas
+vim.opt.number = true         -- Mostrar números de línea
+vim.opt.relativenumber = true -- Números relativos
+vim.opt.expandtab = true      -- Usar espacios en vez de tabulaciones
+vim.opt.shiftwidth = 4        -- Espacios por nivel de indentación
+vim.opt.tabstop = 4           -- Tamaño de un tab
+vim.opt.smartindent = true    -- Indentación inteligente
+vim.opt.hlsearch = false      -- Desactivar el resaltado de búsqueda
+vim.opt.incsearch = true      -- Búsqueda incremental
+vim.opt.termguicolors = true  -- Colores en terminal
+vim.opt.cmdheight = 2         -- Altura de la línea de comandos
+vim.opt.laststatus = 2        -- Siempre mostrar la barra de estado.
+vim.opt.ignorecase = true     -- Ignorar mayúsculas/minúsculas en búsquedas
+vim.opt.smartcase = true      -- Activar búsqueda sensible a mayúsculas
+vim.o.wrap = true
 
 -- Otros ajustes específicos de Neovim
--- vim.opt.clipboard = "unnamedplus"  -- Usar el portapapeles del sistema
+vim.opt.clipboard = "unnamedplus"  -- Usar el portapapeles del sistema
 
 -- Configurar la apertura de ventanas verticales en el lado derecho
 vim.opt.splitright = true
@@ -24,11 +25,7 @@ vim.opt.splitright = true
 vim.opt.splitbelow = true
 
 -- Configuración básica para netrw en Lua
--- vim.g.netrw_banner = 0            -- Deshabilita el banner inicial
-vim.g.netrw_liststyle = 3         -- Usa el modo de árbol
-vim.g.netrw_browse_split = 0      -- Abre en una nueva ventana
-vim.g.netrw_altv = 1              -- Abre en ventanas verticales
-vim.g.netrw_winsize = 75          -- Tamaño de la ventana
+vim.g.netrw_liststyle = 3           -- Usa el modo de árbol
 
 -- Deshabilitar el uso del raton
 vim.opt.mouse = ""
